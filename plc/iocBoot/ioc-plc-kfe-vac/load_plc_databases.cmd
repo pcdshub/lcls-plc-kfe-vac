f19dae71720e6b82ba18b075138b6c1a15429f21
Nolan W. Brown <n-wbrown@users.noreply.github.com>
2020-08-11 17:30:31 -0700

    DEV: update common PV name for standard process

--- plc/iocBoot/ioc-plc-kfe-vac/load_plc_databases.cmd
+++ plc/iocBoot/ioc-plc-kfe-vac/load_plc_databases.cmd
@@ -1,1 +1,1 @@
-dbLoadRecords("plc_kfe_vac.db", "PORT=ASYN_PLC,PREFIX=IOC:PLC:KFE:VAC:,IOCNAME=$(IOCNAME)")
+dbLoadRecords("plc_kfe_vac.db", "PORT=ASYN_PLC,PREFIX=IOC:PLC:KFE:VAC:K0:,IOCNAME=$(IOCNAME)")

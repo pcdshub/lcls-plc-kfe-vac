362de4cdaf7311b540b25e766b2917e17b5b240d
Nolan W. Brown <n-wbrown@users.noreply.github.com>
2020-09-09 22:26:06 -0700

    BUG/BLD: remove unnecessary ':' from prefix

--- plc/iocBoot/ioc-plc-kfe-vac/load_plc_databases.cmd
+++ plc/iocBoot/ioc-plc-kfe-vac/load_plc_databases.cmd
@@ -1,1 +1,1 @@
-dbLoadRecords("plc_kfe_vac.db", "PORT=ASYN_PLC,PREFIX=PLC:KFE:VAC:K0::,IOCNAME=$(IOCNAME)")
+dbLoadRecords("plc_kfe_vac.db", "PORT=ASYN_PLC,PREFIX=PLC:KFE:VAC:K0:,IOCNAME=$(IOCNAME)")

fcb1c28204e324e256db2fcccd5c16cd55434181
Nolan W. Brown <n-wbrown@users.noreply.github.com>
2021-02-05 10:35:50 -0800

    BLD: rebuild IOC

--- plc/iocBoot/ioc-plc-kfe-vac/load_plc_databases.cmd
+++ plc/iocBoot/ioc-plc-kfe-vac/load_plc_databases.cmd
@@ -1,3 +1,3 @@
-# Total records: 4579
-callbackSetQueueSize(11158)
+# Total records: 2918
+callbackSetQueueSize(7836)
 dbLoadRecords("plc_kfe_vac.db", "PORT=ASYN_PLC,PREFIX=PLC:KFE:VAC:K0:,IOCNAME=$(IOC),IOC=$(IOC)")
--- plc/iocBoot/ioc-plc-kfe-vac/st.cmd
+++ plc/iocBoot/ioc-plc-kfe-vac/st.cmd
@@ -5,7 +5,7 @@
 
 epicsEnvSet("ADS_IOC_TOP", "$(TOP)" )
 
-epicsEnvSet("ENGINEER", "mghaly" )
+epicsEnvSet("ENGINEER", "nwbrown" )
 epicsEnvSet("LOCATION", "PLC:KFE:VAC:K0" )
 epicsEnvSet("IOCSH_PS1", "$(IOC)> " )
 epicsEnvSet("ACF_FILE", "$(ADS_IOC_TOP)/iocBoot/templates/unrestricted.acf")

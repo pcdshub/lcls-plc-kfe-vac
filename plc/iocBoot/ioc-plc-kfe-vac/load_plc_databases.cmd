9e7ad185fbc82f0c84fcb6ef355b0fb8abb92d40
Nolan W. Brown <n-wbrown@users.noreply.github.com>
2021-06-30 11:17:32 -0700

    MNT: commit of existing but uncommitted work

--- plc/iocBoot/ioc-plc-kfe-vac/load_plc_databases.cmd
+++ plc/iocBoot/ioc-plc-kfe-vac/load_plc_databases.cmd
@@ -1,3 +1,3 @@
-# Total records: 2922
-callbackSetQueueSize(7844)
+# Total records: 3156
+callbackSetQueueSize(8312)
 dbLoadRecords("plc_kfe_vac.db", "PORT=ASYN_PLC,PREFIX=PLC:KFE:VAC:K0:,IOCNAME=$(IOC),IOC=$(IOC)")

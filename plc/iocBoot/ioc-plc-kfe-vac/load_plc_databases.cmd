ae846b484db8fc07858acc4986f64249a5e050bf
Margaret Ghaly <mghaly@slac.stanford.edu>
2022-08-08 13:47:03 -0700

    MNT: rebuilt IOC with development pytmc

--- plc/iocBoot/ioc-plc-kfe-vac/load_plc_databases.cmd
+++ plc/iocBoot/ioc-plc-kfe-vac/load_plc_databases.cmd
@@ -1,3 +1,3 @@
-# Total records: 3156
-callbackSetQueueSize(8312)
+# Total records: 4716
+callbackSetQueueSize(11432)
 dbLoadRecords("plc_kfe_vac.db", "PORT=ASYN_PLC,PREFIX=PLC:KFE:VAC:K0:,IOCNAME=$(IOC),IOC=$(IOC)")
